b72ca76b398c00fde6ba694a173f969b05241934
Shrizon <shrizon.rex@gmail.com>
2026-02-04 08:55:00 +0600

    smoothing tests

---
+++ tests/test_smooth.py
@@ -0,0 +1,13 @@
+import numpy as np
+from src.infer.smooth import median_smooth
+
+
+def test_kills_single_spike():
+    x = np.array([0, 0, 0, 1, 0, 0, 0], dtype=float)
+    out = median_smooth(x, k=3)
+    assert out[3] == 0
+
+
+def test_k1_identity():
+    x = np.random.randn(10)
+    np.testing.assert_array_equal(median_smooth(x, k=1), x)
